525a15ef30e0da90dbc91fd993c6a303e27d06f1
PikiLee <871565198@qq.com>
2023-09-18 14:21:39 +0800

    refactor: functions

--- routes/[username].ts
+++ routes/[username].ts
@@ -1,152 +1,17 @@
-import { z } from "zod";
-import axios from "axios";
-import sharp from "sharp";
 
-export const getGiuhubData = cachedFunction(async (username: string) => {
-  const GITHUB_CLIENT_TOKEN = process.env.GITHUB_CLIENT_TOKEN;
-  if (!GITHUB_CLIENT_TOKEN)
-    createError({
-      status: 500,
-      message: "Missing GITHUB_CLIENT_TOKEN environment variable",
-    });
-
-  const client = axios.create({
-    baseURL: "https://api.github.com/graphql",
-    headers: {
-      Authorization: `bearer ${GITHUB_CLIENT_TOKEN}`,
-    },
-  });
-
-  const branchSchema = z
-    .object({
-      id: z.string(),
-      target: z.object({
-        oid: z.string(),
-        file: z.object({
-          object: z.object({
-            text: z.string(),
-          }),
-        }),
-      }),
-    })
-    .nullable();
-
-  const readmeSchema = z.object({
-    user: z.object({
-      contributionsCollection: z.object({
-        contributionCalendar: z.object({
-          weeks: z.array(
-            z.object({
-              contributionDays: z.array(
-                z.object({
-                  contributionCount: z.number(),
-                  date: z.string(),
-                })
-              ),
-            })
-          ),
-        }),
-      }),
-    }),
-    repository: z.object({
-      master: branchSchema,
-      main: branchSchema,
-    }),
-  });
-
-  try {
-    const res = await client.post(``, {
-      query: `
-            query($username: String!) {
-              user: user(login: $username) {
-                contributionsCollection {
-                  contributionCalendar {
-                    weeks {
-                      contributionDays {
-                        contributionCount
-                        date
-                      }
-                    }
-                  }
-                }
-              }
-              repository: repository(name: $username, owner: $username) {
-                master: ref(qualifiedName: "master") {
-                  id
-                  target {
-                    ... on Commit {
-                      oid
-                      file(path: "README.md") {
-                        object {
-                          ... on Blob {
-                            text
-                          }
-                        }
-                      }
-                    }
-                  }
-                },
-                main: ref(qualifiedName: "main") {
-                  id
-                  target {
-                    ... on Commit {
-                      oid
-                      file(path: "README.md") {
-                        object {
-                          ... on Blob {
-                            text
-                          }
-                        }
-                      }
-                    }
-                  }
-                }
-              }
-            }
-            `,
-      variables: {
-        username: username,
-      },
-    });
-    // return res.data.data;
-    const data = readmeSchema.parse(res.data.data);
-
-    const contributionDays =
-      data.user.contributionsCollection.contributionCalendar.weeks.flatMap(
-        (week) => week.contributionDays
-      );
-    const daysOnGithub = contributionDays.reduce((acc, day) => {
-      if (day.contributionCount > 0) {
-        acc += 1;
-      }
-      return acc;
-    }, 0);
-    const percentageDaysOnGithub = `${Math.round((daysOnGithub / 365) * 100)}%`;
-
-    const message = `Days on Github in recent 365 days: ${daysOnGithub} / 365, ${percentageDaysOnGithub}`;
-
-    return message;
-  } catch (error) {
-    console.dir(error);
-    createError({
-      status: 500,
-      message: error,
-    });
-  }
-});
+import { generateImageBufferFromText } from "../utils/generateImageBufferFromText";
+import { getDaysOnGithub } from "../utils/getDaysOnGithub";
 
 export default eventHandler(async (event) => {
   const username = getRouterParams(event).username;
-  const message = await getGiuhubData(username);
-  const buffer = await sharp({
-    text: {
-      text: `<span foreground="black">${message}</span>`,
-      rgba: true,
-      dpi: 72,
-    },
-  })
-    .png()
-    .toBuffer();
+
+  const {
+    daysOnGithub,
+    percentageDaysOnGithub,
+  } = await getDaysOnGithub(username);
+  const message = `${daysOnGithub} (${percentageDaysOnGithub}%) days on Github in last 365 days!`;
+
+  const buffer = await generateImageBufferFromText(message);
 
   setResponseHeader(event, "Content-Type", "image/png");
   return buffer;
---
+++ utils/getDaysOnGithub.ts
@@ -0,0 +1,136 @@
+import { z } from "zod";
+import axios from "axios";
+
+export const getDaysOnGithub = cachedFunction(async (username: string) => {
+    const GITHUB_CLIENT_TOKEN = process.env.GITHUB_CLIENT_TOKEN;
+    if (!GITHUB_CLIENT_TOKEN)
+      createError({
+        status: 500,
+        message: "Missing GITHUB_CLIENT_TOKEN environment variable",
+      });
+  
+    const client = axios.create({
+      baseURL: "https://api.github.com/graphql",
+      headers: {
+        Authorization: `bearer ${GITHUB_CLIENT_TOKEN}`,
+      },
+    });
+  
+    const branchSchema = z
+      .object({
+        id: z.string(),
+        target: z.object({
+          oid: z.string(),
+          file: z.object({
+            object: z.object({
+              text: z.string(),
+            }),
+          }),
+        }),
+      })
+      .nullable();
+  
+    const dataSchema = z.object({
+      user: z.object({
+        contributionsCollection: z.object({
+          contributionCalendar: z.object({
+            weeks: z.array(
+              z.object({
+                contributionDays: z.array(
+                  z.object({
+                    contributionCount: z.number(),
+                    date: z.string(),
+                  })
+                ),
+              })
+            ),
+          }),
+        }),
+      }),
+      repository: z.object({
+        master: branchSchema,
+        main: branchSchema,
+      }),
+    });
+  
+    try {
+      const res = await client.post(``, {
+        query: `
+              query($username: String!) {
+                user: user(login: $username) {
+                  contributionsCollection {
+                    contributionCalendar {
+                      weeks {
+                        contributionDays {
+                          contributionCount
+                          date
+                        }
+                      }
+                    }
+                  }
+                }
+                repository: repository(name: $username, owner: $username) {
+                  master: ref(qualifiedName: "master") {
+                    id
+                    target {
+                      ... on Commit {
+                        oid
+                        file(path: "README.md") {
+                          object {
+                            ... on Blob {
+                              text
+                            }
+                          }
+                        }
+                      }
+                    }
+                  },
+                  main: ref(qualifiedName: "main") {
+                    id
+                    target {
+                      ... on Commit {
+                        oid
+                        file(path: "README.md") {
+                          object {
+                            ... on Blob {
+                              text
+                            }
+                          }
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+              `,
+        variables: {
+          username: username,
+        },
+      });
+      const data = dataSchema.parse(res.data.data);
+  
+      const contributionDays =
+        data.user.contributionsCollection.contributionCalendar.weeks.flatMap(
+          (week) => week.contributionDays
+        );
+      const daysOnGithub = contributionDays.reduce((acc, day) => {
+        if (day.contributionCount > 0) {
+          acc += 1;
+        }
+        return acc;
+      }, 0);
+      const percentageDaysOnGithub = `${Math.round((daysOnGithub / 365) * 100)}%`;
+      
+  
+      return {
+        daysOnGithub,
+        percentageDaysOnGithub,
+      }
+    } catch (error) {
+      console.dir(error);
+      createError({
+        status: 500,
+        message: error,
+      });
+    }
+  });
