4c79f78446bf6316a02b472b65eb8fb2051aa747
PikiLee <871565198@qq.com>
2023-09-18 14:44:54 +0800

    fix: response from github should be cached

--- utils/getDaysOnGithub.ts
+++ utils/getDaysOnGithub.ts
@@ -1,61 +1,62 @@
 import { z } from "zod";
 import axios from "axios";
 
-export const getDaysOnGithub = cachedFunction(async (username: string) => {
-  const GITHUB_CLIENT_TOKEN = process.env.GITHUB_CLIENT_TOKEN;
-  if (!GITHUB_CLIENT_TOKEN)
-    createError({
-      status: 500,
-      message: "Missing GITHUB_CLIENT_TOKEN environment variable",
+export const getDaysOnGithub = cachedFunction(
+  async (username: string) => {
+    const GITHUB_CLIENT_TOKEN = process.env.GITHUB_CLIENT_TOKEN;
+    if (!GITHUB_CLIENT_TOKEN)
+      createError({
+        status: 500,
+        message: "Missing GITHUB_CLIENT_TOKEN environment variable",
+      });
+
+    const client = axios.create({
+      baseURL: "https://api.github.com/graphql",
+      headers: {
+        Authorization: `bearer ${GITHUB_CLIENT_TOKEN}`,
+      },
     });
 
-  const client = axios.create({
-    baseURL: "https://api.github.com/graphql",
-    headers: {
-      Authorization: `bearer ${GITHUB_CLIENT_TOKEN}`,
-    },
-  });
+    const branchSchema = z
+      .object({
+        id: z.string(),
+        target: z.object({
+          oid: z.string(),
+          file: z.object({
+            object: z.object({
+              text: z.string(),
+            }),
+          }),
+        }),
+      })
+      .nullable();
 
-  const branchSchema = z
-    .object({
-      id: z.string(),
-      target: z.object({
-        oid: z.string(),
-        file: z.object({
-          object: z.object({
-            text: z.string(),
+    const dataSchema = z.object({
+      user: z.object({
+        contributionsCollection: z.object({
+          contributionCalendar: z.object({
+            weeks: z.array(
+              z.object({
+                contributionDays: z.array(
+                  z.object({
+                    contributionCount: z.number(),
+                    date: z.string(),
+                  }),
+                ),
+              }),
+            ),
           }),
         }),
       }),
-    })
-    .nullable();
-
-  const dataSchema = z.object({
-    user: z.object({
-      contributionsCollection: z.object({
-        contributionCalendar: z.object({
-          weeks: z.array(
-            z.object({
-              contributionDays: z.array(
-                z.object({
-                  contributionCount: z.number(),
-                  date: z.string(),
-                }),
-              ),
-            }),
-          ),
-        }),
+      repository: z.object({
+        master: branchSchema,
+        main: branchSchema,
       }),
-    }),
-    repository: z.object({
-      master: branchSchema,
-      main: branchSchema,
-    }),
-  });
+    });
 
-  try {
-    const res = await client.post(``, {
-      query: `
+    try {
+      const res = await client.post(``, {
+        query: `
               query($username: String!) {
                 user: user(login: $username) {
                   contributionsCollection {
@@ -103,32 +104,38 @@ export const getDaysOnGithub = cachedFunction(async (username: string) => {
                 }
               }
               `,
-      variables: {
-        username,
-      },
-    });
-    const data = dataSchema.parse(res.data.data);
+        variables: {
+          username,
+        },
+      });
+      const data = dataSchema.parse(res.data.data);
 
-    const contributionDays =
-      data.user.contributionsCollection.contributionCalendar.weeks.flatMap(
-        (week) => week.contributionDays,
-      );
-    const daysOnGithub = contributionDays.reduce((acc, day) => {
-      if (day.contributionCount > 0) {
-        acc += 1;
-      }
-      return acc;
-    }, 0);
-    const percentageDaysOnGithub = `${Math.round((daysOnGithub / 365) * 100)}%`;
+      const contributionDays =
+        data.user.contributionsCollection.contributionCalendar.weeks.flatMap(
+          (week) => week.contributionDays,
+        );
+      const daysOnGithub = contributionDays.reduce((acc, day) => {
+        if (day.contributionCount > 0) {
+          acc += 1;
+        }
+        return acc;
+      }, 0);
+      const percentageDaysOnGithub = `${Math.round(
+        (daysOnGithub / 365) * 100,
+      )}%`;
 
-    return {
-      daysOnGithub,
-      percentageDaysOnGithub,
-    };
-  } catch (error) {
-    createError({
-      status: 500,
-      message: error,
-    });
-  }
-});
+      return {
+        daysOnGithub,
+        percentageDaysOnGithub,
+      };
+    } catch (error) {
+      createError({
+        status: 500,
+        message: error,
+      });
+    }
+  },
+  {
+    maxAge: 60 * 60 * 24, // 1 day
+  },
+);
