adac59b9ba817e160d6531ce1eb5e8989571e661
PikiLee <871565198@qq.com>
2024-07-11 15:23:51 +0800

    fix: [App] key

--- src/App.tsx
+++ src/App.tsx
@@ -17,13 +17,14 @@ export default function App({
     <div className="bg-white p-4">
       <h1 className="text-lg font-bold text-green-500 text-center mb-2">{`Spent ${daysOnGithub} (${percentageDaysOnGithub}%) days on Github in last 365 days.`}</h1>
       <div className="grid grid-rows-7 grid-flow-col auto-cols-min gap-0.5">
-        {contributionDays.map(day => {
+        {contributionDays.map((day, index) => {
           const backgroundColorClass =
             day.contributionCount === 0
               ? 'bg-gray-200'
               : backgroundColorClasses[Math.ceil(day.contributionCount / 10)]
           return (
             <div
+              key={index}
               className={clsx(
                 'col-span-1 w-2 aspect-square rounded-[1px] border-gray-800/10 border-[0.8px]',
                 backgroundColorClass
