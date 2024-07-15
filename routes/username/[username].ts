31ae8044179dde22b34445c1432853845de64ea1
PikiLee <871565198@qq.com>
2024-07-15 09:34:08 +0800

    chore: Optimize image generation in username route

--- routes/username/[username].ts
+++ routes/username/[username].ts
@@ -2,26 +2,31 @@ import nodeHtmlToImage from 'node-html-to-image'
 import { getDaysOnGithub } from '../../utils/getDaysOnGithub/getDaysOnGithub'
 import { renderHTML } from '../../utils/renderHTML'
 
-export default defineEventHandler(async event => {
-  const username = getRouterParams(event).username
-  console.log('username', username)
+export default defineCachedEventHandler(
+  async event => {
+    const username = getRouterParams(event).username
+    console.log('username', username)
 
-  const githubData = await getDaysOnGithub(username)
-  if (!githubData) {
-    setResponseStatus(event, 404, 'Not Found')
-    return 'Not Found'
-  }
-
-  const html = await renderHTML(githubData)
-
-  const image = await nodeHtmlToImage({
-    html,
-    puppeteerArgs: {
-      executablePath: '/usr/bin/google-chrome-stable'
+    const githubData = await getDaysOnGithub(username)
+    if (!githubData) {
+      setResponseStatus(event, 404, 'Not Found')
+      return 'Not Found'
     }
-  })
 
-  setResponseHeader(event, 'Content-Type', 'image/png')
+    const html = await renderHTML(githubData)
 
-  return image
-})
+    const image = await nodeHtmlToImage({
+      html,
+      puppeteerArgs: {
+        executablePath: '/usr/bin/google-chrome-stable'
+      }
+    })
+
+    setResponseHeader(event, 'Content-Type', 'image/png')
+
+    return image
+  },
+  {
+    maxAge: 60 * 60 * 24 // 1 day
+  }
+)
