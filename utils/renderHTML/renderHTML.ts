830ff171acd515b5d16b69b39b5e4916953618ee
PikiLee <871565198@qq.com>
2024-08-07 17:17:56 +0800

    refactor: bundle packages when build App

--- utils/renderHTML/renderHTML.ts
+++ utils/renderHTML/renderHTML.ts
@@ -1,5 +1,5 @@
-import { render } from '../../src/render'
 import type { AppProps } from '~/src/App'
+import { render } from '~/dist/server/render.mjs'
 
 export interface GithubData {
   daysOnGithub: number
--- vite.config.mts
+++ vite.config.mts
@@ -8,4 +8,7 @@ export default defineConfig({
     environment: 'jsdom',
     setupFiles: './test.setup.js',
   },
+  ssr: {
+    noExternal: true,
+  },
 })
