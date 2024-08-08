a16d769352b54aed7f3f495cb9c989e635cafc1f
PikiLee <871565198@qq.com>
2024-08-08 11:17:28 +0800

    feat: add @vercel/analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
+import { Analytics } from '@vercel/analytics/react'
+
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div>
+      <Analytics />
       {children}
     </div>
   )
