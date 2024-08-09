fa1af1a75b84a08231ee5d3fe042826e1f97f76c
PikiLee <871565198@qq.com>
2024-08-09 11:31:46 +0800

    feat: [IconSection]

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
 import HomeHero from '~/components/HomeHero'
+import IconSection from '~/components/IconSection'
 
 export default function Page() {
   return (
-    <HomeHero />
+    <>
+      <HomeHero />
+      <IconSection />
+    </>
   )
 }
