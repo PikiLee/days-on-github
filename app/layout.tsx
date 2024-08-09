86579e7260c17860ada16d39213c64e9829f964a
PikiLee <871565198@qq.com>
2024-08-09 11:07:10 +0800

    feat: header component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,13 @@
 import { Analytics } from '@vercel/analytics/react'
 import '../index.css'
+import Header from '~/components/Header'
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html>
       <body>
         <Analytics />
+        <Header />
         <main>{children}</main>
       </body>
     </html>
---
+++ components/Header.tsx
@@ -0,0 +1,44 @@
+'use client'
+
+import Link from 'next/link'
+import { LuSquareStack } from 'react-icons/lu'
+import { FaGithub } from 'react-icons/fa'
+import { RxHamburgerMenu } from 'react-icons/rx'
+import { useState } from 'react'
+
+export default function Header() {
+  const [navOpen, setNavOpen] = useState(false)
+
+  return (
+    <header className="fixed top-0 inset-x-0 flex flex-wrap md:justify-start md:flex-nowrap z-50 w-full text-sm">
+      <nav className="mt-4 relative max-w-2xl w-full bg-white border border-gray-200 rounded-[2rem] mx-2 py-2.5 md:flex md:items-center md:justify-between md:py-0 md:px-4 md:mx-auto dark:bg-neutral-900 dark:border-neutral-700">
+        <div className="px-4 md:px-0 flex justify-between items-center">
+          <div>
+            <Link className="flex items-center w-max gap-1 rounded-md text-xl font-semibold focus:outline-none focus:opacity-80" href="/" aria-label="Days on Github">
+              <LuSquareStack size={24} />
+              Days on Github
+            </Link>
+          </div>
+
+          <div className="md:hidden">
+            <button type="button" className="flex justify-center items-center size-6 border border-gray-200 text-gray-500 rounded-full hover:bg-gray-200 focus:outline-none focus:bg-gray-200 dark:border-neutral-700 dark:text-neutral-400 dark:hover:bg-neutral-700 dark:focus:bg-neutral-700" aria-expanded="false" aria-label="Toggle navigation" onClick={() => setNavOpen(prev => !prev)}>
+              <RxHamburgerMenu />
+            </button>
+          </div>
+        </div>
+
+        {navOpen && (
+          <div className="overflow-hidden transition-all duration-300 basis-full grow md:flex md:justify-end md:gap-4">
+            <div className="flex flex-col md:flex-row md:items-center md:justify-end gap-2 md:gap-3 mt-3 md:mt-0 py-2 md:py-0 md:ps-7">
+              <Link className="py-0.5 md:py-3 px-4 md:px-1 border-s-2 md:border-s-0 md:border-b-2 border-gray-800 font-medium text-gray-800 focus:outline-none dark:border-neutral-200 dark:text-neutral-200" href="/" aria-current="page">Home</Link>
+            </div>
+            <div className="before:block before:absolute before:top-1/2 before:-start-px before:w-px before:h-4 before:bg-gray-300 before:-translate-y-1/2 dark:before:bg-neutral-700 flex max-md:justify-center max-md:mt-4 items-center relative pl-4">
+              <Link href="https://github.com/PikiLee/days-on-github" target="_blank"><FaGithub size={18} /></Link>
+            </div>
+          </div>
+        )}
+      </nav>
+    </header>
+
+  )
+}
