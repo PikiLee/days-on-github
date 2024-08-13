dbfd543c2c1ceb18a7c958e2ffb79193cc6bed58
PikiLee <871565198@qq.com>
2024-08-13 20:51:21 +0800

    fix: [tailwind] add utils folder to content paths

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,10 +5,21 @@ import { nextui } from '@nextui-org/react'
 module.exports = {
   content: {
     relative: true,
-    files: ['./{app,components}/**/*.{js,jsx,ts,tsx}', './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}'],
+    files: ['./{app,components,utils}/**/*.{js,jsx,ts,tsx}', './node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}'],
   },
   darkMode: 'class',
   plugins: [
-    nextui(),
+    nextui({
+      themes: {
+        light: {
+          colors: {
+            primary: {
+              foreground: '#FFFFFF',
+              DEFAULT: '#12A150',
+            },
+          },
+        },
+      },
+    }),
   ],
 }
