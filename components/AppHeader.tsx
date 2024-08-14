8d9fa187a2803cf8f92dcfa5d72f0ee1cf042d90
PikiLee <871565198@qq.com>
2024-08-14 11:44:39 +0800

    feat: [docs] page

--- components/AppHeader.tsx
+++ components/AppHeader.tsx
@@ -5,9 +5,20 @@ import { LuSquareStack } from 'react-icons/lu'
 import { IoLogoGithub, IoMenuOutline } from 'react-icons/io5'
 import { useState } from 'react'
 import clsx from 'clsx'
+import { usePathname } from 'next/navigation'
 
 export default function AppHeader() {
   const [navOpen, setNavOpen] = useState(false)
+  const pathname = usePathname()
+  const links = [{
+    href: '/',
+    label: 'Home',
+    isActive: pathname === '/',
+  }, {
+    href: '/docs',
+    label: 'Documentation',
+    isActive: pathname === '/docs',
+  }]
 
   return (
     <header className="sticky top-0 inset-x-0 flex flex-wrap md:justify-start md:flex-nowrap z-50 w-full text-sm">
@@ -29,7 +40,11 @@ export default function AppHeader() {
 
         <div className={clsx('overflow-hidden transition-all duration-300 basis-full grow md:flex md:justify-end md:gap-4', navOpen ? 'block' : 'hidden')}>
           <div className="flex flex-col md:flex-row md:items-center md:justify-end gap-2 md:gap-3 mt-3 md:mt-0 py-2 md:py-0 md:ps-7">
-            <Link className="py-0.5 md:py-3 px-4 md:px-1 border-s-2 md:border-s-0 md:border-b-2 border-gray-800 font-medium text-gray-800 focus:outline-none dark:border-neutral-200 dark:text-neutral-200" href="/" aria-current="page">Home</Link>
+            {links.map(({ href, label, isActive }) => (
+              <Link className={clsx('py-0.5 md:py-3 px-4 md:px-1 md:border-s-0 border-gray-800 font-medium text-gray-800 focus:outline-none dark:border-neutral-200 dark:text-neutral-200', isActive && 'border-s-2 md:border-b-2')} key={href} href={href}>
+                {label}
+              </Link>
+            ))}
           </div>
           <div className="before:block before:absolute before:top-1/2 before:-start-px before:w-px before:h-4 before:bg-gray-300 before:-translate-y-1/2 dark:before:bg-neutral-700 flex max-md:justify-center max-md:mt-4 items-center relative pl-4">
             <Link href="https://github.com/PikiLee/days-on-github" target="_blank"><IoLogoGithub size={18} /></Link>
