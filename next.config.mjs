f201d54accbb160ddc03f49614e89c2c8adc56b5
PikiLee <871565198@qq.com>
2024-08-17 10:51:47 +0800

    chore: add mdx

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,12 @@
 // @ts-check
+import createMDX from '@next/mdx'
 
 /**
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
+  // Configure `pageExtensions` to include markdown and MDX files
+  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
   /* config options here */
   redirects: async () => {
     return [
@@ -22,4 +25,9 @@ const nextConfig = {
   },
 }
 
-export default nextConfig
+const withMDX = createMDX({
+  // Add markdown plugins here, as desired
+})
+
+// Merge MDX config with Next.js config
+export default withMDX(nextConfig)
