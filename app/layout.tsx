e9f771d47b830b87296a8a17d84b8c5e7605304b
PikiLee <871565198@qq.com>
2024-08-17 13:26:14 +0800

    refactor: add font

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,14 @@ import { Analytics } from '@vercel/analytics/react'
 import '../index.css'
 import { NextUIProvider } from '@nextui-org/react'
 import type { Metadata } from 'next'
+import { Nunito } from 'next/font/google'
 import AppHeader from '~/components/AppHeader'
 import AppFooter from '~/components/AppFooter'
+
+const font = Nunito({
+  subsets: ['latin'],
+  display: 'swap',
+})
 
 export const metadata: Metadata = {
   title: 'Days on Github',
@@ -12,7 +18,7 @@ export const metadata: Metadata = {
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html>
+    <html lang="en" className={font.className}>
       <body>
         <Analytics />
         <NextUIProvider>
